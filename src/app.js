3745afd46ea33f7c96252a4c35905a9a6c9cb217
Monika Mitka <monikacodes@gmail.com>
2020-11-18 14:47:15 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`
+	}
+	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let day = days[date.getDay()];
+	return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
-	console.log(response.data)
+	let dateElement = document.querySelector("#date");
+	dateElement.innerHTML = formatDate(response.data.dt * 1000)
 	let windElement = document.querySelector("#wind");
 	windElement.innerHTML = Math.round(response.data.wind.speed);
 	let humidityElement = document.querySelector("#humidity");
