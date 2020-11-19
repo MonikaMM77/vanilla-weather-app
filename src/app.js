c18370107d42002edcc8100afda09c720a88ce36
Monika Mitka <monikacodes@gmail.com>
2020-11-19 14:38:27 +0100

    add the search engine

--- src/app.js
+++ src/app.js
@@ -31,10 +31,21 @@ function displayTemperature(response) {
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
-let city = "Brisbane";
-let apiKey = "30d51b8f5d573674a85c2b8f5f80916d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+	let apiKey = "30d51b8f5d573674a85c2b8f5f80916d";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+	event.preventDefault();
+	let cityInputElement = document.querySelector("#city-input");
+	search(cityInputElement.value);
+	console.log(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
